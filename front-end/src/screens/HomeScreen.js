4ffe448101a0132627fcdde2837004d1d906f71c
denosy <50819494+denosy@users.noreply.github.com>
2021-02-21 20:32:02 +0300

    git commit

--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,19 +1,28 @@
-import React, { useEffect, useState } from 'react'
+//import React, { useEffect, useState } from 'react'
+import React, { useEffect} from 'react'
 import Product from "../components/Product"
-import axios from "axios"
+//import axios from "axios"
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from "../components/MessageBox"
+import { useDispatch, useSelector } from 'react-redux';
+import { listProducts } from '../actions/productAction';
 
 export default function HomeScreen() {
-    //define a react hook to manage the state of your react component 
+    
+    //to get product from redux store we use useselector
+    const productList = useSelector( state => state.productList);
+    const {loading, error, products} = productList;
+    //to import dispatch
+    const dispatch = useDispatch();
+
+    {/** define a react hook to manage the state of your react component 
     const [products, setProducts] = useState([]);
-
     const [loading, setLoading] = useState(false);
-
     const [error, setError] = useState(false);
+    **/}
 
     // Use effect function hook accept two parameter, a function and array that accept list of dependency
-    useEffect(() => {
+    {/**useEffect(() => {
         //a function to fetch data
         const fetchData = async () => {
             try{
@@ -25,12 +34,15 @@ export default function HomeScreen() {
             } catch(err){
                 setError(err.message);
                 setLoading(false)
-            }
-
-            
+            }         
         };
         fetchData();
     }, [])
+    **/}
+    
+    useEffect(() => {
+        dispatch(listProducts());
+    }, []);
 
     return (
         <div>
---
+++ front-end/src/store.js
@@ -0,0 +1,20 @@
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
+import thunk from "redux-thunk"
+import { productListReducer } from './reducers/productReducer';
+
+const initialState = {};
+
+//A reducer is a function that accept two parameter
+{/**const reducer = (state, action) => {
+    return{ products: data.products};
+**/}
+
+const reducer = combineReducers({
+    productList: productListReducer,
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
