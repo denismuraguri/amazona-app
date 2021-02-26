d5ed7f1ac57628a446a950c65436e8564a031f34
denosy <50819494+denosy@users.noreply.github.com>
2021-02-26 23:50:32 +0300

    share

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,13 +4,24 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import {Link} from 'react-router-dom'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import SigninScreen from './screens/SigninScreen';
+import { signout } from './actions/userActions';
+import RegisterScreen from './screens/RegisterScreen';
+
 /**</BrowserRouter> */
 function App() {
 
     const cart = useSelector(state => state.cart)
     const{ cartItems } = cart;
+    const userSignin = useSelector((state) => state.userSignin);
+    const { userInfo } = userSignin;
+    const dispatch = useDispatch();
+
+    const signoutHandler = () => {
+        dispatch(signout());
+
+    }
 
   return (
       <BrowserRouter >
@@ -25,9 +36,23 @@ function App() {
                         <span className="badge">{cartItems.length}</span>
                     )}   
                     </Link>
-                    <Link to="/signin">
-                        sign-in
-                    </Link>
+                    {
+                        userInfo ? (
+                            <div className="dropdown">
+                                <Link to="#">{userInfo.name} <i className="fa fa-caret-down"></i> {' '}</Link>
+                                <ul className="dropdown-content">
+                                    <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+                                </ul>
+                            </div>
+                        ) :
+                        (
+                            <Link to="/signin">
+                                 sign-in
+                            </Link>
+                        )
+
+                    }
+                    
                 </div>
 
             </header>
@@ -35,6 +60,7 @@ function App() {
                 <Route path="/cart/:id?" component={CartScreen}></Route>
                 <Route path="/product/:id" component={ProductScreen}></Route>
                 <Route path="/signin" component={SigninScreen}></Route>
+                <Route path="/register" component={RegisterScreen}></Route>
                 <Route path="/" component={HomeScreen} exact>
 
                 </Route>
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -3,8 +3,13 @@ import thunk from "redux-thunk"
 import { cartReducer } from './reducers/cartReducers';
 import { productDetailsReducer, 
     productListReducer } from './reducers/productReducer';
+import { userRegisterReducer, userSigninReducer } from './reducers/userReducers';
 
 const initialState = {
+    userSignin: {
+        userInfo: localStorage.getItem('userInfo')? JSON.parse(localStorage.getItem('userInfo'))
+        : null,
+    },
     cart:{
         cartItems: localStorage.getItem('cartItems')? JSON.parse(localStorage.getItem('cartItems'))
         : [],
@@ -12,14 +17,16 @@ const initialState = {
 };
 
 //A reducer is a function that accept two parameter
-{/**const reducer = (state, action) => {
+/**const reducer = (state, action) => {
     return{ products: data.products};
-**/}
+**/
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
+    userSignin: userSigninReducer,
+    userRegister: userRegisterReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
